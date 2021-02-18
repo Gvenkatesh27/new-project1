bd829c9cb9e4c11271370047bc90f60d98bf0a81
venky <gvenkyram@gmail.com>
2021-02-18 16:15:25 +0530

    react

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,92 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-import People from './People';
-import Users from './Users';
-import Person from './components/Person';
-import React,{Component, useState} from 'react';
-import MyHeader from './MyHeader';
-import Input from './Input';
+import Comments from "./components/comments/Comments";
+import Approval from "./components/comments/approval/Approval";
+import faker from "faker";
 
-
-
-const App = () => {
-  const [userstate,setuserstate] = useState(
-    {
-    user_deatils : [
-      { "name" : "suresh", "mobile" : "7674905234"},
-      { "name" : "ranjith", "mobile" : "7674905234"},
-      { "name" : "pavan", "mobile" : "7674905234"}
-    ],
-    show_data : false
-  }
-  );
-
-  var style ={
-    backgroundColor : "red",
-    color:"white"
-  }
-
-const buttonclick = () =>{
-    setuserstate({
-      user_deatils : [
-        { "name" : "kumar", "mobile" : "7674905234"},
-        { "name" : "saipav", "mobile" : "7674905234"},
-        { "name" : "kalyan", "mobile" : "7674905234"}
-      ],
-      show_data : true
-    })
-  }
-
-  const deleteData = (i) => {
-    console.log(i);
-    var myuser = userstate.user_deatils;
-    myuser.splice(i,1);
-    setuserstate({
-      user_deatils:myuser
-    })
-    console.log(userstate.user_deatils);
-  }
-
-  let newperson = "";
-  // if(userstate.show_data === true){
-    newperson = (
-      // <Person name = "mounisha" mobile = "123456789"/>
-      <div>
-        {userstate.user_deatils.map((v,i) => {
-          return<Person name ={v.name} mobile ={v.mobile} key= {i} click = {() => deleteData(i)}/>;
-        })}
+const App = () =>{
+  return(
+    <div className="App">
+      <div className="ui comments">
+        <Approval>
+        <Comments name="MASTER" comment="FLOP" time="Today at 5:30pm" image={faker.image.image()}></Comments>
+        </Approval>
+        <Approval>
+        <Comments name="VALIMAI" comment="BLOCK BUSTER" time="Today at 7:30pm" image={faker.image.image()}></Comments>
+        </Approval>
+        <Approval>
+        <Comments name="COBRA" comment="AVERAGE" time="Today at 9:30pm" image={faker.image.image()}></Comments>
+        </Approval>
+        <Approval>
+        <Comments name="DON" comment="HIT" time="Today at 10:30pm" image={faker.image.image()}></Comments>
+        </Approval>
       </div>
-    )
-  // }
 
-  var button_classes = ["hide"];
-  if(userstate.user_deatils.length <=0){
-    button_classes.splice(0,1);
-    button_classes.push("show");
-  }
-
-    return(
-      <div className="App">
-      {/* <Person name ={userstate.user_deatils[0].name} mobile={userstate.user_deatils[0].mobile} />
-      <Person name ={userstate.user_deatils[1].name} mobile={userstate.user_deatils[1].mobile} />
-      <Person name ={userstate.user_deatils[2].name} mobile={userstate.user_deatils[2].mobile} /> */}
-      <button className="btn">click this button</button><br/>
-      <button  style ={style} onClick = {buttonclick}>showdata</button>
-      <p className={button_classes.join(" ")}>No data found</p>
-      {newperson}
-      <People/>
-      <div className="ui grid">
-  <div className="four wide column">pavan</div>
-  <div className="four wide column">ranjith</div>
-  <div className="four wide column">suresh</div>
-  <div className="four wide column">venky</div>
-  <div className="two wide column">moni</div>
-  <div className="eight wide column">priya</div>
-  <div className="six wide column">sam</div>
-</div>
-<Users/>
     </div>
-    );
+  );
 }
-
 export default App;
