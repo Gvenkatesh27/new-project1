8adacef8c95adcf735956d1dffe0e6608b56a18c
venky <gvenkyram@gmail.com>
2021-02-24 12:39:27 +0530

    react

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -3,6 +3,8 @@ import Comments from "./components/comments/Comments";
 import Approval from "./components/comments/approval/Approval";
 import faker from "faker";
 import  React,{ Component , useState} from 'react';
+import Loader from './components/comments/loader/Loader';
+import Wheather from './components/comments/wheather/Wheather';
 
 class App extends Component{
   constructor(props){
@@ -14,8 +16,8 @@ class App extends Component{
           long : null
         }
       ],
+      errorMsg : ''
     }
-    
     window.navigator.geolocation.getCurrentPosition(position => {
       console.log(position)
       this.setState({
@@ -27,21 +29,37 @@ class App extends Component{
     },
       error => {
         console.log(error)
+        this.setState({
+          errorMsg:error.message
+        })
       }
     )
   }
   
-  
+  componentWillUpdate(){
+    console.log("Component did update called");
+    console.log("done with geolocation");
+  }
 
   render() {
+    console.log("Component rerendered");
+    if(!this.state.errorMsg && this.state.pos.lat){
         return(
-          <div className="App">
-          hello your coords<br/><br/>
-          {console.log(this.state)}
-          latitude : {this.state.pos.lat},<br/><br/>
-          longitude : {this.state.pos.long},
-          </div>
+          // <div className="App">
+          //   HELLO YOUR COORDS ARE<br/><br/>
+          //   {console.log(this.state)}
+          //   latitude : {this.state.pos.lat},<br/><br/>
+          //   longitude : {this.state.pos.long}
+          // </div>
+          <Wheather lat={this.state.pos.lat}/>
         );
+    }     
+    
+    if(this.state.errorMsg && !this.state.pos.lat){
+      return <div>Error: {this.state.errorMsg}</div>
+    }
+
+     return <Loader/>;
   }
-}
+};
 export default App;
