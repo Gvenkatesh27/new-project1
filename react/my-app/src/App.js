ea01c056f7c4ab548474d143229c4f4e55c9c321
venky <gvenkyram@gmail.com>
2021-03-03 15:41:46 +0530

    react

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -3,9 +3,16 @@ import Comments from "./components/comments/Comments";
 import  React,{ Component , useState} from 'react';
 import Search from "./components/comments/search/Search";
 import axios from "axios";
+import SearchResults from "./components/comments/searchr/serchResults/SearchResults";
 
 
 class App extends Component{
+
+
+  state = {
+    search_query : null,
+    url_link : []
+  }
 
 
   APIurl ='https://api.unsplash.com/photos?page=1&query=office&client_id=nJCG928Ha0pnILd7T_EmXwDO45tiHLwktHhZeBs-3Ko';
@@ -19,10 +26,12 @@ class App extends Component{
       }
     })
 
-    .then(function (response) {
-      console.log(response);
+    .then( response =>{
+      console.log(response.data);
+      this.setState({url_link : response.data});
+      console.log(this.state);
     })
-    .catch(function (error) {
+    .catch( error => {
       console.log(error);
     })
     .then(function () {
@@ -31,15 +40,43 @@ class App extends Component{
   }
 
   onGetData =(mydata) => {
+    this.setState({search_query : mydata})
     console.log(mydata);
     this.loaddata(mydata);
   }
 
+  html_temp = "";
+
+
+  load_search_data = () => {
+    this.html_temp = this.state.url_link.map(function(v,i){
+      // console.log(v);
+      var data = <SearchResults img_link={v.urls.regular} name ={v.alt_description}></SearchResults>
+      return data;
+    });
+    return this.html_temp;
+  }
+
 
   render(){
+    var search_display_res ;
+    if(this.state.search_query != null){
+      search_display_res = <div><h2>Search Results for {this.state.search_query}</h2><SearchResults></SearchResults></div>
+    }
     return(
-      <div className="App">
+      <div className="ui container">
         <Search onSubmit={this.onGetData}/>
+        <div>
+          {/* <h2>Search Results for {this.state.search_query}</h2> */}
+          {/* <SearchResults></SearchResults> */}
+          {/* <hr></hr> */}
+          {search_display_res}
+          
+          <div className="ui three column grid">
+            {/* {this.html_temp} */}
+            {this.load_search_data()};
+          </div>
+        </div>      
       </div>
     )
   }
