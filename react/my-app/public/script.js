129b3556a7652eed3a16d1797ccbddcddd2b5e7d
venky <gvenkyram@gmail.com>
2021-04-15 15:00:09 +0530

    axios

--- react/my-app/public/script.js
+++ react/my-app/public/script.js
@@ -1,3 +1,20 @@
-$(document).on("click",".btn",function(params){
-    alert("vesath*");
-});
+// $(document).on("click",".btn",function(params){
+//     alert("vesath*");
+// });
+
+
+// let img = document.querySelector('img');
+// let btn = document.querySelector('.btn');
+
+// btn.addEventListener('click', () => {
+//     img.src = 'jewwllery.jpg';
+// })
+
+var myImage = document.getElementById('mainImage');
+var imageArray = ["jewwllery.jpg", "boys.jpg"];
+var imageIndex = 1;
+
+function changeImage(){
+    myImage.setAttribute("src",imageArray[imageIndex]);
+    imageIndex++;
+}
--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,155 +1,126 @@
+import React, { Component } from "react";
 import './App.css';
-import Comments from "./components/comments/Comments";
-import  React,{ Component , useState} from 'react';
-import Search from "./components/comments/search/Search";
-import axios from "axios";
-import SearchResults from "./components/comments/searchr/serchResults/SearchResults";
+import Usestyles from "./components/Usestyles";
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Product from "./components/Product";
+import Usestyle from "./components/Usestyle";
+// import { List } from "@material-ui/core";
+import List from "./components/List";
 
 
-class App extends Component{
-
-
-  state = {
-    search_query : null,
-    url_link : []
+const list = [
+  {
+    title: "kani",
+    content: "cook with comali 2",
+    Television : "vijaytv"
+  },
+  {
+    title: "Ashwin kumar",
+    content: "cook with comali 2",
+    Television :"vijaytv"
+  },
+  {
+    title: "shakilla",
+    content: "cook with comali 2",
+    Television : "vijaytv"
   }
-
-
-  APIurl ='https://api.unsplash.com/photos?page=1&query=office&client_id=nJCG928Ha0pnILd7T_EmXwDO45tiHLwktHhZeBs-3Ko';
-
-  loaddata =(q_data) =>{
-    axios.get('https://api.unsplash.com/photos?', {
-      params: {
-      page : 1,
-      query : q_data,
-      client_id : "nJCG928Ha0pnILd7T_EmXwDO45tiHLwktHhZeBs-3Ko"
-      }
-    })
-
-    .then( response =>{
-      console.log(response.data);
-      this.setState({url_link : response.data});
-      console.log(this.state);
-    })
-    .catch( error => {
-      console.log(error);
-    })
-    .then(function () {
-      // always executed
-    }); 
-  }
-
-  onGetData =(mydata) => {
-    this.setState({search_query : mydata})
-    console.log(mydata);
-    this.loaddata(mydata);
-  }
-
-  html_temp = "";
-
-
-  load_search_data = () => {
-    this.html_temp = this.state.url_link.map(function(v,i){
-      // console.log(v);
-      var data = <SearchResults img_link={v.urls.regular} name ={v.alt_description}></SearchResults>
-      return data;
-    });
-    return this.html_temp;
-  }
-
-
-  render(){
-    var search_display_res ;
-    if(this.state.search_query != null){
-      search_display_res = <div><h2>Search Results for {this.state.search_query}</h2><SearchResults></SearchResults></div>
-    }
-    return(
-      <div className="ui container">
-        <Search onSubmit={this.onGetData}/>
-        <div>
-          {/* <h2>Search Results for {this.state.search_query}</h2> */}
-          {/* <SearchResults></SearchResults> */}
-          {/* <hr></hr> */}
-          {search_display_res}
-          
-          <div className="ui three column grid">
-            {/* {this.html_temp} */}
-            {this.load_search_data()};
-          </div>
-        </div>      
+];
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <List items={list} />
       </div>
     )
   }
-};
+}
 
-
-
-
-
-
-
-
-
-
-
+// const App = () => {
+//   const [count, newCount] = useState(0);
+//   const incCount = () => {
+//     console.log("button clicked");
+//     newCount(count+1);
+//   }
+//   return (
+//     <div>
+//       <div>
+//         <button onClick={() => incCount()}>Inc Count</button>
+//       </div>
+//       <div>
+//         My count value = {count}
+//       </div>
+//     </div>
+//   )
+// }
 
 
 
 
 // class App extends Component{
-//   constructor(props){
-//     super(props)
-//     this.state = {
-//       pos : [
-//         {
-//           lat : null,
-//           long : null
-//         }
-//       ],
-//       errorMsg : ''
-//     }
-//     window.navigator.geolocation.getCurrentPosition(position => {
-//       console.log(position)
-//       this.setState({
-//         pos : {
-//           lat :position.coords.latitude,
-//           long : position.coords.longitude
-//         }
-//       })
-//     },
-//       error => {
-//         console.log(error)
-//         this.setState({
-//           errorMsg:error.message
-//         })
-//       }
+//   render(){
+//     return(
+//       <div className="App">
+//       <Usestyles/>
+//       <Usestyle/>
+//       </div>
 //     )
 //   }
-  
-//   componentWillUpdate(){
-//     console.log("Component did update called");
-//     console.log("done with geolocation");
+// }
+
+
+// class App extends Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       items: [],
+//       isLoaded: false,
+//     }
+//   }
+
+//   componentDidMount() {
+//     fetch('https://fakestoreapi.com/products/category/jewelery')
+//       .then(res => res.json())
+//       .then(json => {
+//         this.setState({
+//           isLoaded: true,
+//           items: json,
+//         })
+//       });
 //   }
 
 //   render() {
-//     console.log("Component rerendered");
-//     if(!this.state.errorMsg && this.state.pos.lat){
-//         return(
-//           // <div className="App">
-//           //   HELLO YOUR COORDS ARE<br/><br/>
-//           //   {console.log(this.state)}
-//           //   latitude : {this.state.pos.lat},<br/><br/>
-//           //   longitude : {this.state.pos.long}
-//           // </div>
-//           <Wheather lat={this.state.pos.lat}/>
-//         );
-//     }     
-    
-//     if(this.state.errorMsg && !this.state.pos.lat){
-//       return <div>Error: {this.state.errorMsg}</div>
-//     }
 
-//      return <Loader/>;
+//     var { isLoaded, items } = this.state;
+//     if (!isLoaded) {
+//       return <div>Loading...</div>;
+//     }
+//     else {
+//       return (
+//         <div className="App">
+//           <Usestyles></Usestyles>
+//           {/* <Product/> */}
+//           <ul>
+//               {items.map(item => (
+//               <li Id ={item.id}>
+//                 <table Id ={item.id}>
+//                 Title : {item.title} | Price : {item.price} | Description : {item.description} | Category : {item.category} | Image : {item.image}
+//                 </table>
+//               </li>
+//               ))};
+//             </ul>
+//         </div>
+//       );
+//     }
 //   }
-// };
+// }
+
+
 export default App;
+
+
+
+
+
