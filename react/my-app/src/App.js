6d23c27677c2e82ce1c23f1ed2fc106ad2f100cf
venky <gvenkyram@gmail.com>
2021-04-19 15:52:01 +0530

    frontend display

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -12,13 +12,17 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
+
 const StyledTableCell = withStyles((theme) => ({
   head: {
     backgroundColor: theme.palette.common.black,
     color: theme.palette.common.white,
+    fontSize : "x-large",
+    fontFamily : "italic"
   },
   body: {
-    fontSize: 14,
+    fontSize: "x-large",
+    fontFamily : "italic"
   },
 }))(TableCell);
 
@@ -36,56 +40,19 @@ const useStyles = makeStyles({
   },
 });
 
+const mystyle = {
+  color: "black",
+  backgroundColor: "white",
+  padding: "10px",
+  fontFamily: "Arial",
+  float : "right"
 
+};
 
-
-// const list = [
-//   {
-//     title: "kani",
-//     content: "cook with comali 2",
-//     Television : "vijaytv"
-//   },
-//   {
-//     title: "Ashwin kumar",
-//     content: "cook with comali 2",
-//     Television :"vijaytv"
-//   },
-//   {
-//     title: "shakilla",
-//     content: "cook with comali 2",
-//     Television : "vijaytv"
-//   }
-// ];
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <List items={list} />
-//       </div>
-//     )
-//   }
-// }
-
-// const App = () => {
-//   const [count, newCount] = useState(0);
-//   const incCount = () => {
-//     console.log("button clicked");
-//     newCount(count+1);
-//   }
-//   return (
-//     <div>
-//       <div>
-//         <button onClick={() => incCount()}>Inc Count</button>
-//       </div>
-//       <div>
-//         My count value = {count}
-//       </div>
-//     </div>
-//   )
-// }
-
-
-
+const mystyles = {
+  backgroundColor : "orange",
+  color : "white"
+}
 
 
 
@@ -95,10 +62,12 @@ const useStyles = makeStyles({
 const App = () => {
   const classes = useStyles();
   const [product, setProduct] = useState([]);
+  const [search, setSearch] = useState("");
+
   const getProductData = async () => {
     try {
       // const data = await axios.get("https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=pavan"
-      const data = await axios.get("https://fakestoreapi.com/products/category/jewelery"
+      const data = await axios.get("https://jsonplaceholder.typicode.com/users"
       );
       console.log(data.data);
       setProduct(data.data);
@@ -110,41 +79,71 @@ const App = () => {
   useEffect(() => {
     getProductData();
   }, []);
+
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <center><h1>REACTJS</h1></center>
       {/* {product.map((item) => {
         return <p>{item.id}-- {item.title}-- {item.price}</p>
       })} */}
+      <input
+        type="text"
+        placeholder="Search here" style={mystyle}  
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      />
       <TableContainer component={Paper}>
         <Table className={classes.table} aria-label="customized table">
           <TableHead>
             <TableRow>
-            <StyledTableCell>Id</StyledTableCell>
-              <StyledTableCell>Title</StyledTableCell>
-              <StyledTableCell>Price</StyledTableCell>
-              <StyledTableCell>Description</StyledTableCell>
+              <StyledTableCell>Id</StyledTableCell>
+              <StyledTableCell>Name</StyledTableCell>
+              <StyledTableCell>Email</StyledTableCell>
+              <StyledTableCell>Phone</StyledTableCell>
+              <StyledTableCell>City</StyledTableCell>
+              <StyledTableCell>CompanyName</StyledTableCell>
+              <StyledTableCell>More Info Links</StyledTableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {product.map((item) => {
-              return (
-                <StyledTableRow key ={item.id}>
-                  <StyledTableCell component="th" scope="row">
-                    {item.id}
-                  </StyledTableCell>
-                  <StyledTableCell component="th" scope="row">
-                    {item.title}
-                  </StyledTableCell>
-                  <StyledTableCell align="right">
-                    {item.price}
-                  </StyledTableCell>
-                  <StyledTableCell component="th" scope="row">
-                    {item.description}
-                  </StyledTableCell>
-                </StyledTableRow>
-              );
-            })};
+            {product.filter((item) => {
+              if (search == "") {
+                return item;
+              } else if (
+                // item.title.toLowerCase().includes(search.toLowerCase())
+                item.description.toLowerCase().includes(search.toLowerCase())
+              ) {
+                return item;
+              }
+            })
+              .map((item) => {
+                return (
+                  <StyledTableRow key={item.id}>
+                    <StyledTableCell component="th" scope="row">
+                      {item.id}
+                    </StyledTableCell>
+                    <StyledTableCell component="th" scope="row">
+                      {item.name}
+                    </StyledTableCell>
+                    <StyledTableCell component="th" scope="row">
+                      {item.email}
+                    </StyledTableCell>
+                    <StyledTableCell component="th" scope="row">
+                      {item.phone}
+                    </StyledTableCell>
+                    <StyledTableCell component="th" scope="row">
+                      {item.address.city}
+                    </StyledTableCell>
+                    <StyledTableCell component="th" scope="row">
+                      {item.company.name}
+                    </StyledTableCell>
+                    <StyledTableCell component="th" scope="row">
+                      <button style={mystyles}>More Info</button>
+                    </StyledTableCell>
+                  </StyledTableRow>
+                );
+              })};
           </TableBody>
         </Table>
       </TableContainer>
